27f014337f5aa5b8464a7d64ae84ea411711d8fa
Saurabh Sharma <saurabh98048@gmail.com>
2025-06-30 05:38:01 +0530

    fck this sht

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // 🔥 REPO NAME = 'SYMPTOM-SENSEI-AI-CHAT'
 export default defineConfig(({ mode }) => ({
-  base: '/medical-assistant/',
+  base: '/medical-assistant.github.io/',
   server: {
     host: "::",
     port: 8080,
